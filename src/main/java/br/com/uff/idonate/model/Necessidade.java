970abc9145519689ee680f640eaa9570c7b55ff7
jorgeaugustocarvalho <javgccarvalho@gmail.com>
2022-05-15 19:55:17 -0300

    fix nome das tabelas

--- src/main/java/br/com/uff/idonate/model/Necessidade.java
+++ src/main/java/br/com/uff/idonate/model/Necessidade.java
@@ -1,26 +1,24 @@
 package br.com.uff.idonate.model;
 
 import java.util.Date;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
+import javax.persistence.*;
+
 import lombok.Data;
 
 /** Classe modelo de necessidade que deve ser informada pelo voluntário. */
 @Data
 @Entity
+@Table(name = "necessidades")
 public class Necessidade {
 
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private long id;
 
-  @JoinColumn(name = "instituicao")
+  @JoinColumn(name = "instituicoes")
   private Instituicao instituicao;
 
-  @JoinColumn(name = "voluntario")
+  @JoinColumn(name = "voluntarios")
   private Voluntario voluntario;
 
   private Categoria categoria;
--- src/main/java/br/com/uff/idonate/model/Voluntario.java
+++ src/main/java/br/com/uff/idonate/model/Voluntario.java
@@ -2,15 +2,14 @@ package br.com.uff.idonate.model;
 
 import jakarta.persistence.ManyToMany;
 import java.util.List;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
+
 import lombok.Data;
 
 /** Classe modelo da Entidade Voluntário do Idonate. */
 @Data
 @Entity
+@Table(name = "voluntarios")
 public class Voluntario {
 
   @Id

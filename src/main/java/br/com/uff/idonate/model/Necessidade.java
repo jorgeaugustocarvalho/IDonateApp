9717e50ae498f7f8e7ba1424c27c9ed480f7072b
jorgeaugustocarvalho <javgccarvalho@gmail.com>
2022-05-15 22:06:17 -0300

    expand imports

--- src/main/java/br/com/uff/idonate/model/Necessidade.java
+++ src/main/java/br/com/uff/idonate/model/Necessidade.java
@@ -1,8 +1,12 @@
 package br.com.uff.idonate.model;
 
 import java.util.Date;
-import javax.persistence.*;
-
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import javax.persistence.Id;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.JoinColumn;
 import lombok.Data;
 
 /** Classe modelo de necessidade que deve ser informada pelo voluntário. */
